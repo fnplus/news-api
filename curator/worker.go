da96af5792f94fc86b513dd90d3a10c10ecf5c7a
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-01-01 16:55:48 +0530

    Created interface and structure for a Worker

--- curator/worker.go
+++ curator/worker.go
@@ -6,8 +6,26 @@ import (
 	"github.com/barthr/newsapi"
 )
 
-func getNewsWithKeywords(keywords string) ([]newsapi.Article, error) {
-	client := newsapi.NewClient("3111edae78f6492983bd0a6df945356e")
+// IWorker interface to work with multiple sources of news and put it all into
+// the data store
+type IWorker interface {
+	GetNewsWithKeywords(keywords string) ([]newsapi.Article, error)
+}
+
+// NewWorker creates a new instance of iWorker
+func NewWorker(newsAPIKey string) IWorker {
+	return &Worker{newsAPIKey: newsAPIKey}
+}
+
+// Worker implements the iWorker interface
+type Worker struct {
+	newsAPIKey string
+}
+
+// GetNewsWithKeywords gets the news from the newsapi source, for the given
+// keywords
+func (w *Worker) GetNewsWithKeywords(keywords string) ([]newsapi.Article, error) {
+	client := newsapi.NewClient(w.newsAPIKey) // Change API key
 	ar, err := client.GetEverything(context.Background(), &newsapi.EverythingParameters{
 		Keywords: keywords,
 		Language: "en",
--- curator/worker_test.go
+++ curator/worker_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 )
 
-func Test_runWorker(t *testing.T) {
-	_, err := getNewsWithKeywords("python")
+func TestWorker_GetNewsWithKeywords(t *testing.T) {
+	apikey := "3111edae78f6492983bd0a6df945356e"
+	worker := NewWorker(apikey)
+	_, err := worker.GetNewsWithKeywords("golang")
 	if err != nil {
 		t.Fatalf("Test failed!\nError: %s", err.Error())
 	}
